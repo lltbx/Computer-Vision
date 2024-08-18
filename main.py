faadc9b1de8a836419200d306d7595dce4eb50ad
Mykhailo Virshylo <mishavirshylo@gmail.com>
2024-08-18 22:56:07 +0300

    py changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union , Annotated
 
 from fastapi import FastAPI , File , UploadFile
 
@@ -8,6 +8,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Drag or drop the image "}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
